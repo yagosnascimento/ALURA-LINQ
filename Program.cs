246b6765b4c125a4da98726f97e8cee4e8da6f79
Yago Nascimento <yagosiqueira450@gmail.com>
2025-12-29 21:06:43 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+        Console.ReadKey();
+    }
+    catch (Exception ex)
+    {
+               Console.WriteLine("Ocorreu um erro: " + ex.Message);
+    }
+}
